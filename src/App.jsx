ebf28fd9435acb5228fa1e50ab3c797dee61e082
Daniel Almeida <almeida.danny@gmail.com>
2025-10-07 13:46:11 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+      <head>
+      <title>MusicOrg</title> 
+      </head>
+      <body>
+        <h1>Hello World!</h1>
+      </body>
+   </>
+  )
+}
+
+export default App
